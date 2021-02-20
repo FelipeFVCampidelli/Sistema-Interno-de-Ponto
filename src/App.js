0ebfd64b6f501e8b00f6a0daa3885d794462af21
FelipeFVCampidelli <felipefvcampidelli@hotmail.com>
2021-02-20 13:49:52 -0300

    ultima Versão

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import React, { Fragment, useEffect, useState } from "react"
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom"
+import { BrowserRouter as Router, Link, Route, Switch, Redirect } from "react-router-dom"
 import Axios from 'axios';
 import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
@@ -22,15 +22,32 @@ export default function App() {
 
   const name = 'EXEMPLO'
 
+  const [login, setLogin] = useState(false);
+  const [diretor, setDiretor] = useState(false);
+
+  function redirectToLogin() {
+    if (!login) {
+      return <Redirect to='/login'></Redirect>;
+    }
+  }
+  
+  function redirectToPerfil(){
+    if (login) {
+      return <Redirect to='/perfil' ></Redirect>;
+    }
+    if (login && diretor) {
+      return <Redirect to='/diretor' ></Redirect>;
+    }
+  }
+  
   //GET
-  const [funcionario, setFuncionario] = useState([])
+  let [funcionario, setFuncionario] = useState( [{funcionarios: {} }] );
   useEffect(() => {
     Axios.get("http://localhost:4001/user/search").then(res => {
-      console.log(res.data)
       setFuncionario(res.data)
-    })
-  })
-
+    }).catch((err) => { console.error("ops! ocorreu um erro" + err);})
+  }, [])//{funcionario[0].username}
+  
   return (
       
     <Router>
@@ -47,17 +64,17 @@ export default function App() {
           <Form inline>
             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
             <Button variant="outline-light">Search</Button>
-          </Form>
+          </Form> 
         </Navbar>
         <br />
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path="/diretor" component={Diretor} />
-          <Route path="/login" component={Login} />
+          <Route path="/diretor" component={Diretor}>{redirectToLogin()}</Route>
+          <Route path="/login" component={Login}>{redirectToPerfil()}</Route>
           <Route path="/cadastro" component={Cadastro} />
           <Route path="/edit" component={Edit} />
           <Route path="/delete" component={Delete} />
-          <Route path="/perfil/:name" component={Perfil} />
+          <Route path="/perfil/:name" component={Perfil}>{redirectToLogin()}</Route>
           <Route path="/ponto" component={Ponto} />
           <Route render={() => <h1>404: página não encontrada</h1>} />
         </Switch>
--- src/Login/login.jsx
+++ src/Login/login.jsx
@@ -1,28 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
+import Axios from 'axios';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 import './login.css';
 
 export default function Login() {
+
+  const [login, setLogin] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  function handlePasswordChange(event) { setPassword(event.target.value);}
+  function handleNameChange(event) { setEmail(event.target.value);}
+  
+  
+  //POST
+  function handleSubmit(event){
+    const pacotinho = [
+      {
+        email: email,
+        password: password
+      }
+    ]  
+    Axios.post("http://localhost:4001/user/login", pacotinho)
+    .then((res) => {
+      if(res.status === 200){
+        setLogin(true);
+      }else{
+        alert(res.data);
+      }
+    })
+    .catch(function (err){console.log(err);})
+  }
+
   return (
     <div>
       <Container fluid>
         <Row>
           <Col>
             <h1>Página de Login</h1>
-            <Form>
+            <Form method="POST" onSubmit={handleSubmit}>
               <Form.Group controlId="formBasicEmail">
                 <Form.Label>Nome de usuário</Form.Label>
-                <Form.Control type="username" placeholder="Insíra seu nome de usuário" required />
+                <Form.Control type="username" placeholder="Insíra seu nome de usuário"  onChange={handleNameChange} required />
               </Form.Group>
               <Form.Group controlId="formBasicPassword">
                 <Form.Label>Senha</Form.Label>
-                <Form.Control type="password" placeholder="Insira sua senha" required />
+                <Form.Control type="password" placeholder="Insira sua senha" onChange={handlePasswordChange} required />
               </Form.Group>
-              <Button variant="primary" type="submit">
-                Logar
-              </Button>
+              <Button variant="primary" type="submit">Logar</Button>
             </Form>
           </Col>
         </Row>
